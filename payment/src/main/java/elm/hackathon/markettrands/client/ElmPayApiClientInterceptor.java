8174a8185a7f2d9dfa038d9ba6fa438cb7958ca2
hniazy <honiazy@elm.com.sa>
2025-12-06 16:00:21 +0300

    fein client

--- payment/src/main/java/elm/hackathon/markettrands/client/ElmPayApiClientInterceptor.java
+++ payment/src/main/java/elm/hackathon/markettrands/client/ElmPayApiClientInterceptor.java
@@ -8,6 +8,7 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 
 public class ElmPayApiClientInterceptor implements RequestInterceptor {
@@ -18,7 +19,7 @@ public class ElmPayApiClientInterceptor implements RequestInterceptor {
         requestTemplate.header("Content-Type", "application/json");
         requestTemplate.header("ProductCode", "817");
         requestTemplate.header("ClientKey", "4E012D6C-7B3E-4D3A-9890-5E3C0815EA12");
-        requestTemplate.header("MessageId", "Market Trends Subscription");
+        requestTemplate.header("MessageId", UUID.randomUUID().toString());
         requestTemplate.header("Timestamp", LocalDateTime.now().toString());
         requestTemplate.header("app-id", "8ab5d791");
         requestTemplate.header("app-key", "77a0e640a57cf7e166acfcb4645ddd8c");
--- payment/src/main/java/elm/hackathon/markettrands/client/billing/ElmBillingPayApiClientInterceptor.java
+++ payment/src/main/java/elm/hackathon/markettrands/client/billing/ElmBillingPayApiClientInterceptor.java
@@ -2,8 +2,10 @@ package elm.hackathon.markettrands.client.billing;
 
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+import org.hibernate.id.GUIDGenerator;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 
 public class ElmBillingPayApiClientInterceptor implements RequestInterceptor {
@@ -14,7 +16,7 @@ public class ElmBillingPayApiClientInterceptor implements RequestInterceptor {
         requestTemplate.header("Content-Type", "application/json");
         requestTemplate.header("ProductCode", "817");
         requestTemplate.header("ClientKey", "9e7364b7-f5a4-4495-8746-7fe6b341973c");
-        requestTemplate.header("MessageId", "Market Trends Subscription");
+        requestTemplate.header("MessageId", UUID.randomUUID().toString());
         requestTemplate.header("Timestamp", LocalDateTime.now().toString());
         requestTemplate.header("app-id", "a26c555d");
         requestTemplate.header("app-key", "c440f95c0be4981f8c2d2a465e83edcb");
