a9cd08474cb7d51f05587321919a91a6d340f1e2
hniazy <honiazy@elm.com.sa>
2025-12-06 13:34:26 +0300

    fein client

--- payment/src/main/java/elm/hackathon/markettrands/PaymentApplication.java
+++ payment/src/main/java/elm/hackathon/markettrands/PaymentApplication.java
@@ -1,10 +1,11 @@
-package elm.hackathon.markettrands.markettrends;
+package elm.hackathon.markettrands;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
-//@EnableFeignClient
+@EnableFeignClients
 public class PaymentApplication {
 
 	public static void main(String[] args) {
